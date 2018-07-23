c7ab2936e35c2740a5e735f3846cdf56ba86ee12
chenwenjian <chenwenjian@leadeon.cn>
2018-07-23 09:24:20 +0800

    add Music.component

--- src/app/front-end/music/MusicHome.component.ts
+++ src/app/front-end/music/MusicHome.component.ts
@@ -2,14 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   template: `<div>
-      <div nz-row>
-          <div nz-col nzSpan="8">
-              <app-slide-nav></app-slide-nav>
-          </div>
-          <div nz-col nzSpan="16">
-              <div class="music-content"></div>
-          </div>
-      </div>
+          <app-slide-nav></app-slide-nav>
+          <div class="music-content"></div>
       <app-play-control></app-play-control>
   </div>`,
   styleUrls: ['./MusicHome.component.css']
